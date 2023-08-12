c6015744435f8697f0ad7378445abc1e872ecacf
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-12 09:16:30 +0000

    modified some  files

--- Js/index.js
+++ Js/index.js
@@ -1,50 +1,52 @@
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.getElementById('menu-btn');
-const closeBtn = document.getElementById('close-btn');
-const sideMenuButton = document.querySelectorAll(".sidebar a");
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const sideMenuButton = document.querySelectorAll(".sidebar");
 let valueDisplay = document.querySelectorAll(".number");
 // let setInterval = document.querySelectorAll(".percent")
 
-const darkMode = document.querySelector('.dark-mode');
+const darkMode = document.querySelector(".dark-mode");
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
+menuBtn.addEventListener("click", () => {
+	sideMenu.style.display = "block";
 });
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+	sideMenu.style.display = "none";
 });
 
-darkMode.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode-variables');
-    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-})
+darkMode.addEventListener("click", () => {
+	document.body.classList.toggle("dark-mode-variables");
+	darkMode.querySelector("span:nth-child(1)").classList.toggle("active");
+	darkMode.querySelector("span:nth-child(2)").classList.toggle("active");
+});
 
-
-Orders.forEach(order => {
-    const tr = document.createElement('tr');
-    const trContent = `
+Orders.forEach((order) => {
+	const tr = document.createElement("tr");
+	const trContent = `
         <td>${order.productName}</td>
         <td>${order.productNumber}</td>
         <td>${order.paymentStatus}</td>
-        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="${
+					order.status === "Declined"
+						? "danger"
+						: order.status === "Pending"
+						? "warning"
+						: "primary"
+				}">${order.status}</td>
         <td class="primary">Details</td>
     `;
-    tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
+	tr.innerHTML = trContent;
+	document.querySelector("table tbody").appendChild(tr);
 });
 
 function handleButtonClick(event) {
-	// Get the clicked button element
 	const clickedButton = event.target;
 
 	const sidebar = document.querySelector(".sidebar");
 
-	// Get the currently active button (if any)
 	const activeButton = sidebar.querySelector(".active");
 
-	// Remove the active class from the previously active button
 	if (activeButton) {
 		activeButton.classList.remove("active");
 	}
@@ -69,10 +71,7 @@ sideMenuButton.forEach((button) =>
 	button.addEventListener("click", handleSidebarButtonClick)
 );
 
-
-
-
-let interval = 7000;
+let interval = 70000;
 
 valueDisplay.forEach((value) => {
 	let startValue = 0;
@@ -82,11 +81,7 @@ valueDisplay.forEach((value) => {
 		startValue += 1;
 		value.textContent = startValue;
 		if (startValue == endValue) {
-			clearInterval(counter)
+			clearInterval(counter);
 		}
-	},duration );
-}
-
-);
-
-
+	}, duration);
+});
