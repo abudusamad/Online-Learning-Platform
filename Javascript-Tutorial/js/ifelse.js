a826517174a1bbc156eedb42e7e666989ce11b30
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-22 23:32:57 +0000

    Adding condition to the functions

--- Javascript-Tutorial/js/ifelse.js
+++ Javascript-Tutorial/js/ifelse.js
@@ -3,3 +3,11 @@ const para = document.querySelector("p");
 
 
 select.addEventListener("change", setWeather);
+
+function setWeather() {
+    const choice = select.value;
+
+    if (choice === "sunny") {
+        para.textContent = "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
+    }
+}
