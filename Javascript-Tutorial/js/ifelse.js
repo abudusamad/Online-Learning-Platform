9ee8a119c7bc949761fea5562392277fde5373e3
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-22 23:37:55 +0000

    Finished with the conditioanal statement for the code

--- Javascript-Tutorial/js/ifelse.js
+++ Javascript-Tutorial/js/ifelse.js
@@ -11,5 +11,9 @@ function setWeather() {
         para.textContent = "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
     } else if (choice === "rainy") {
         para.textContent = "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long."
+    } else if (choice === "overcast") {
+        para.textContent = "It isn't raining,but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case."
+    } else {
+        para.textContent = "";
     }
 }
