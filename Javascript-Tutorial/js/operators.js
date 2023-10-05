36a5b149a09a10d1976ab1a8f446592d779d9a3e
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 21:34:09 +0000

    Adding operators to the tutorial sections

--- Javascript-Tutorial/js/operators.js
+++ Javascript-Tutorial/js/operators.js
@@ -8,3 +8,29 @@ result = firstNumber + secondNumber
 
 const myResult = document.querySelector("h1")
 myResult.textContent = result
+
+
+// Substraction
+
+
+firstNumber = 23
+secondNumber = 2
+
+result = firstNumber - secondNumber
+
+
+// Division
+
+firstNumber = 34
+secondNumber = 45
+
+result = secondNumber / firstNumber 
+
+
+
+// mutiplication
+
+firstNumber = 23
+secondNumber = 12
+
+result = firstNumber * secondNumber
