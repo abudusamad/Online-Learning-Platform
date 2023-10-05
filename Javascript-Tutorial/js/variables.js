8bb9782daabcda6505d3c0822493ec5d619e078f
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 23:17:32 +0000

    working with the dom of the javascript element

--- Javascript-Tutorial/js/variables.js
+++ Javascript-Tutorial/js/variables.js
@@ -17,4 +17,11 @@ myVariable = [2, "Bobs", 435, "Kofi ", "bring me the money"]
 
 //Objects
 
-myVariable = document.querySelector("h1")
+const button = document.querySelector("button");
+
+button.addEventListener("click", updateName);
+
+function updateName() {
+    const name = prompt("Enter your name");
+    button.textContent = `Player 1: ${name}`
+}
