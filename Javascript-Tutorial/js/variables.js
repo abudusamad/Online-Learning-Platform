e5460b6c1d87222044774ca51abcc4bf4ce5c23d
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 21:22:27 +0000

    showing the various datatype within a variavble

--- Javascript-Tutorial/js/variables.js
+++ Javascript-Tutorial/js/variables.js
@@ -1,2 +1,16 @@
 let myVariable;
 myVariable = "Bob";
+
+///String
+myVariable = "Bobs";
+
+//Boolean
+
+myVariable = false
+
+//Numbers = 
+myVariable = 2343
+
+//Array
+
+myVariable = [ 2, "Bobs", 435, "Kofi ", "bring me the money"]
