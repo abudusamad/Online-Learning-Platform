ee58650766265a61ed12766c0249069e1bee8245
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 21:39:52 +0000

    working with conditional statement with js

---
+++ Javascript-Tutorial/js/condtional.js
@@ -0,0 +1,11 @@
+// Adding condition statement for decision making within the code
+// using if else statement in decision making
+
+
+let iceCreame = "cholocate"
+if (iceCreame === "cholcate") {
+    alert("Yah, I Love Icreame ")
+}
+else {
+    alert("Hey, I thought is an icrecream oo")
+}
