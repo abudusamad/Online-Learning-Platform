4dd9a90976c46f757a486666e7fe248b5b09b475
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-05 21:30:55 +0000

    working with the various operators within the operators

---
+++ Javascript-Tutorial/js/operators.js
@@ -0,0 +1,10 @@
+// Addition
+let firstNumber, secondNumber, result
+
+firstNumber = 4
+secondNumber = 6
+
+result = firstNumber + secondNumber
+
+const myResult = document.querySelector("h1")
+myResult.textContent = result
