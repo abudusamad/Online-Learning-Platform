6f65e2a3b0cdd47a6be6beaa5bf6bd35a208644c
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 20:26:11 +0000

    Adding dom manipulation to the program after to make interactive guesses

---
+++ Javascript-Tutorial/js/guessing.js
@@ -0,0 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1; 
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+
+function checkGuess() {
+    const userGuess = Number(guessField.ariaValueMax);
+    if (guessCount == 1) {
+        guesses.textContent = "Previous Guesses: ";
+    }
+    guesses.textContent = `${guesses.textContent} ${userGuess}`
+}
