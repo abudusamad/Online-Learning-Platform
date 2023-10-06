afd440eea4b5363ae8b678974bd3868039e21f68
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 21:10:49 +0000

    working with the dom

--- Javascript-Tutorial/js/main.js
+++ Javascript-Tutorial/js/main.js
@@ -1,2 +1,12 @@
 const myHeading = document.querySelector('h1')
-myHeading.textContent= "hello world"
+myHeading.textContent = "hello world"
+
+const headingA = document.querySelector("#heading_A");
+const buttonA = document.querySelector("#button_A");
+
+
+buttonA.onclick = () => {
+    const name = prompt("What is your name?");
+    alert(`Hello ${name} nice to meet you`);
+    headingA.textContent = `You're welcome ${name}`
+}
