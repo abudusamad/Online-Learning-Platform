0e6c984a8407b347abdc17e1d8a57a824a856327
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 22:32:27 +0000

    modfied some files

--- Javascript-Tutorial/js/main.js
+++ Javascript-Tutorial/js/main.js
@@ -5,6 +5,22 @@ myHeading.textContent = "hello world"
 const headingA = document.querySelector("#heading_A");
 const buttonA = document.querySelector("#button_A");
 
+const btn = document.querySelector("button");
+const txt = document.querySelector("p");
+
+
+btn.addEventListener("click", checkButton);
+
+function checkButton() {
+    if (btn.textContent === "start machine") {
+        btn.textContent = "The machine is started"
+        txt.textContent = "The machine is working"
+    } else {
+        btn.textContent = "Start Machine";
+        txt.textContent = "Machine is Stopped"
+    }
+}
+
 
 
 buttonA.onclick = () => {
