3ae1feba11e86f324e3a5e07aebe79ed15993d17
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 20:39:23 +0000

    Adding the function for the reset button for the game

--- Javascript-Tutorial/js/guessing.js
+++ Javascript-Tutorial/js/guessing.js
@@ -17,5 +17,42 @@ function checkGuess() {
     if (guessCount == 1) {
         guesses.textContent = "Previous Guesses: ";
     }
-    guesses.textContent = `${guesses.textContent} ${userGuess}`
+    guesses.textContent = `${guesses.textContent} ${userGuess}`;
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Congratulations! You got it right!"
+        lastResult.style.backgroundColor = "greeen";
+        lowOrHi.textContent = "";
+        setGameOver();
+    }
+    else if (guessCount === 10) {
+        lastResult.textContent = "!!! Game Over !!!";
+        lowOrHi.textContent = "";
+        setGameOver();
+
+    } else {
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = "Last  guess was too low!";
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = "Last guess was too High!";
+        }
+    }
+
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
+    
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Start new game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame)
 }
