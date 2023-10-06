e8f819c6df231512b3b6f9bc409333f05fb038cc
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 21:46:02 +0000

    Adding another text to text the functionality of the code

--- Javascript-Tutorial/js/main.js
+++ Javascript-Tutorial/js/main.js
@@ -1,12 +1,14 @@
 const myHeading = document.querySelector('h1')
 myHeading.textContent = "hello world"
 
+
 const headingA = document.querySelector("#heading_A");
 const buttonA = document.querySelector("#button_A");
 
 
+
 buttonA.onclick = () => {
-    const name = prompt("What is your name?");
+    const name = prompt("What is your name? ");
     alert(`Hello ${name} nice to meet you`);
-    headingA.textContent = `You're welcome ${name}`
+    headingA.textContent= `You're Welcome ${name}`
 }
