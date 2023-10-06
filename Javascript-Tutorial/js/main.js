0ae5a32d05513ec861d2ef136c70348b2be8cb19
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 22:50:42 +0000

    A simple function to double an item of an array

--- Javascript-Tutorial/js/main.js
+++ Javascript-Tutorial/js/main.js
@@ -35,6 +35,7 @@ const cities = ["Manchester", "Liverpool", "Edinburgh", "Carlistle"];
 const index = cities.indexOf("Liverpool");
 if (index !== -1) {
     cities.splice(index, 1)
+    console.log(cities)
 }
 
 //Accessing every item of the array using for loop
@@ -45,3 +46,13 @@ for (bird of birds) {
     console.log(bird)
 }
 
+//A simple code to the double an item
+// using a function
+
+function double(number) {
+    return number * 2;
+}
+
+const numbers = [4, 5, 7, 20, 5];
+const doubled = numbers.map(double);
+console.log(doubled)
