fb185369776c3920be3d1545bbcdc24ee6d1af6d
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-06 20:51:00 +0000

    okay

--- Javascript-Tutorial/js/guessing.js
+++ Javascript-Tutorial/js/guessing.js
@@ -1,4 +1,4 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1; 
+let randomNumber = Math.floor(Math.random() * 10) + 1; 
 
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
