efaafea89f367361874150b6f905613addd82397
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-07 11:13:43 +0000

    Adding loops to the code

---
+++ Javascript-Tutorial/js/loops.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector("button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d")
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+})
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < 100; i++){
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(200,0,0,0.5)";
+        ctx.arc(
+            random(canvas.width),
+            random(canvas.height),
+            random(50),
+            0,
+            2 * Math.PI
+        );
+        ctx.fill();
+    }
+}
+
+btn.addEventListener("click", draw);
